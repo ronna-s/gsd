59d3ac8b69e52d47b3e675557b472b831e55fc21
ronna-s <ronna.steinberg@gmail.com>
2019-07-19 14:04:19 +0200

    from the middl

--- main.go
+++ main.go
@@ -1,31 +1,20 @@
 package main
 
 import (
-	"context"
 	"io"
 	"log"
 	"net"
-	"os"
-	"os/signal"
 	"sync"
-	"syscall"
 )
 
-func handle(conn net.Conn, ctx context.Context) {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	go func() {
-		<-ctx.Done()
-		conn.Close()
-	}()
-
+func handle(conn net.Conn) {
 	if _, err := io.Copy(conn, conn); err != nil {
 		log.Println(err)
 	}
+	conn.Close()
 }
 
-func serve(l net.Listener, ctx context.Context) error {
+func serve(l net.Listener) error {
 	var wg sync.WaitGroup
 	var conn net.Conn
 	var err error
@@ -37,43 +26,23 @@ func serve(l net.Listener, ctx context.Context) error {
 		wg.Add(1)
 		go func(c net.Conn) {
 			defer wg.Done()
-			handle(c, ctx)
+			handle(c)
 		}(conn)
 	}
 	wg.Wait()
 	return err
 }
 
-func listenAndServe(network, address string, ctx context.Context, ready chan struct{}) error {
+func listenAndServe(network, address string) error {
 	l, err := net.Listen(network, address)
 	if err != nil {
 		return err
 	}
-	close(ready)
-	go func() {
-		<-ctx.Done()
-		l.Close()
-	}()
 	defer l.Close()
 
-	return serve(l, ctx)
+	return serve(l)
 }
 
 func main() {
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	go func() {
-		<-sigCh
-		cancel()
-	}()
-	ready := make(chan struct{})
-	go func() {
-		<-ready
-		log.Println("ready to accept connections")
-	}()
-
-	log.Fatal(listenAndServe("tcp", ":9090", ctx, ready))
+	log.Fatal(listenAndServe("tcp", ":9090"))
 }
